d5652a00375635fcb2b6a5e3d2ae41b85239df55
kohei240155 <k.pino519@gmail.com>
2024-07-23 20:15:27 +0900

    Add SelectionSort

---
+++ sort/selectionSort/SelectionSort1.java
@@ -0,0 +1,30 @@
+package selectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort1 {
+
+	public static void selectionSort(int[] array) {
+		int n = array.length;
+		
+		for (int i = 0; i < n - 1; i++) {
+			int minIndex = i;
+			for (int j = i + 1; j < n; j++) {
+				if (array[j] < array[minIndex]) {
+					minIndex = j;
+				}
+			}
+			
+			int temp = array[minIndex];
+			array[minIndex] = array[i];
+			array[i] = temp;
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {3, 1, 2, 5, 9, 2, 2, 4, 6, 2, 1, 3, 9};
+		selectionSort(array);
+		System.out.println(Arrays.toString(array));
+	}
+
+}
