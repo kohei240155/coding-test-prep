78de631e6357e1f9200f76d6766ad4f914e74476
Kohei Fujiwara <k.pino519@gmail.com>
2026-04-03 23:39:25 -0700

    add: implement variable assignment and logging examples in practice3.js

---
+++ 2026-04-03/practice3.js
@@ -0,0 +1,21 @@
+let originalNumber = 42;
+let copiedNumber = originalNumber;
+
+copiedNumber = 100;
+
+console.log('originalNumber:', originalNumber);
+console.log('copiedNumber:', copiedNumber);
+
+let originalString = 'hello';
+let copiedString = originalString;
+
+copiedString = 'world';
+
+console.log('originalString:', originalString);
+console.log('copiedString:', copiedString);
+
+let price = 1000;
+let discountedPrice = price;
+discountedPrice = discountedPrice * 0.8;
+console.log('price:', price);
+console.log('discountedPrice:', discountedPrice);
