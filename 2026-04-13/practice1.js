4552f5db99ec4414324d20bccaf309565e0e6409
Kohei Fujiwara <k.pino519@gmail.com>
2026-04-13 08:50:51 -0700

    remove: comment out main execution flow in practice1.js

--- 2026-04-13/practice1.js
+++ 2026-04-13/practice1.js
@@ -14,10 +14,26 @@
 // heavyTask('重い処理B');
 // console.log('main: 全て完了');
 
-console.log('1: main 開始');
+// console.log('1: main 開始');
 
-setTimeout(function delayedTask() {
-  console.log('2: setTimeout のコールバック実行');
-}, 0);
+// setTimeout(function delayedTask() {
+//   console.log('2: setTimeout のコールバック実行');
+// }, 0);
 
 console.log('3: main 終了');
+
+console.log('1: script 開始（同期）');
+
+setTimeout(function macroTask() {
+  console.log('5: setTimeout コールバック（マクロタスク）');
+}, 0);
+
+Promise.resolve().then(function microTask() {
+  console.log('3: Promise.then コールバック（マイクロタスク）');
+});
+
+queueMicrotask(function anotherMicroTask() {
+  console.log('4: queueMicrotask コールバック（マイクロタスク）');
+});
+
+console.log('2: script 終了（同期）');
