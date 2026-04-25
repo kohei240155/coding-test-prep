2bfd3c96095ca69e52c38702d6f70e12401bc082
Kohei Fujiwara <k.pino519@gmail.com>
2026-04-25 12:27:27 -0700

    Add various algorithm implementations and examples
    - Implemented maxProfit function to calculate maximum profit from stock prices.
    - Added twoSum function to find indices of two numbers that add up to a target.
    - Created examples demonstrating variable scope, object copying, and array manipulation.
    - Developed isPalindrome function to check if a string is a palindrome.
    - Implemented canConstruct function to determine if a ransom note can be constructed from a magazine.
    - Added backspaceCompare function to compare two strings after applying backspace operations.
    - Implemented mergeTwoLists function to merge two sorted linked lists.
    - Created hasCycle function to detect a cycle in a linked list.
    - Added HTML and JavaScript example for demonstrating microtask vs macrotask execution.
    - Implemented binary search algorithm for searching elements in an array.

---
+++ temp.js
@@ -0,0 +1,33 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({ input: process.stdin });
+const lines = [];
+
+rl.on("line", (line) => lines.push(line.trim()));
+rl.on("close", () => {
+  const n = parseInt(lines[0]);
+  const S = lines[1].split(" ").map(Number);
+  const q = parseInt(lines[2]);
+  const T = lines[3].split(" ").map(Number);
+
+  // Binary Search
+  function binarySearch(key) {
+    let left = 0;
+    let right = n;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (S[mid] === key) return true;
+      else if (key > S[mid]) left = mid + 1; // search right half
+      else right = mid; // search left half
+    }
+    return false;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < q; i++) {
+    if (binarySearch(T[i])) sum++;
+  }
+
+  console.log(sum);
+});
